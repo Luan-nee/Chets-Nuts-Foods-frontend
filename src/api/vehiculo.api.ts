76f4275aa07fe6e826d9a4c8469a13045cb896e6
Luan-nee <luandelsol54@gmail.com>
2026-02-09 19:27:29 -0500

    implementación del modo offline para el endpoint de vehiculos, importando los datos desde un archivo json y retornandolos en la función get, además de agregar una nueva variable privada para controlar el modo offline

--- src/api/vehiculo.api.ts
+++ src/api/vehiculo.api.ts
@@ -3,16 +3,21 @@ import { url_base } from "../config/url_base";
 import BaseRequestApi from './BaseRequest.api';
 // importación de tipos
 import type { BodyResponse } from '../types/bodyResponse.type';
+import dataVehiculos from '../json/vehiculos/get-ok-listarVehiculos.json';
 
 export default class VehiculoApi extends BaseRequestApi {
   private base_url = `${url_base}/vehiculos`;
 
   public async get<T>(): Promise<BodyResponse<T>> {
-    return this.request<T>(`${this.base_url}`, {
-      method: 'GET',
-      headers: {
-        'x-mock-response-name': 'ok - listar vehiculos'
-      }
-    });
+    if (this.OFFLINE_MODE) {
+      return dataVehiculos as unknown as BodyResponse<T>;
+    } else {
+      return this.request<T>(`${this.base_url}`, {
+        method: 'GET',
+        headers: {
+          'x-mock-response-name': 'ok - listar vehiculos'
+        }
+      });
+    }
   }
 }
