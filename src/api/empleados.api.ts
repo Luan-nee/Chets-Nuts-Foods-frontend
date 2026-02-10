c32f3abd2b55903ff7376169fc2982e8e3f43b06
Luan-nee <luandelsol54@gmail.com>
2026-02-09 20:33:06 -0500

    implementación de modo offline para visualizar datos de la interfaz.

--- src/api/empleados.api.ts
+++ src/api/empleados.api.ts
@@ -3,16 +3,22 @@ import { url_base } from "../config/url_base";
 import BaseRequestApi from './BaseRequest.api';
 // importación de tipos
 import type { BodyResponse } from '../types/bodyResponse.type';
+import dataEmpleados from '../json/empleados/get-ok-listarEmpleados.json';
 
 export default class EmpleadoApi extends BaseRequestApi {
   private base_url = `${url_base}/empleados`;
 
   public async get<T>(): Promise<BodyResponse<T>> {
-    return this.request<T>(`${this.base_url}`, {
-      method: 'GET',
-      headers: {
-        'x-mock-response-name': 'ok - listar empleados'
-      }
-    });
+
+    if (this.OFFLINE_MODE) {
+      return dataEmpleados as unknown as BodyResponse<T>;
+    } else {
+      return this.request<T>(`${this.base_url}`, {
+        method: 'GET',
+        headers: {
+          'x-mock-response-name': 'ok - listar empleados'
+        }
+      });
+    }
   }
 }
--- src/api/gre.api.ts
+++ src/api/gre.api.ts
@@ -3,25 +3,35 @@ import { url_base } from "../config/url_base";
 import BaseRequestApi from './BaseRequest.api';
 // importación de tipos
 import type { BodyResponse } from '../types/bodyResponse.type';
+import dataGre from '../json/guiaderemision/get-ok-guiasDeRemision.json';
+import dataGreDetalle from '../json/guiaderemision/get-ok-detallesDeUnaGuiaDeRemision.json';
 
 export default class GreApi extends BaseRequestApi {
   private base_url = `${url_base}/guiasEmision`;
 
   public async get<T>(): Promise<BodyResponse<T>> {
-    return this.request<T>(`${this.base_url}`, {
-      method: 'GET',
-      headers: {
-        'x-mock-response-name': 'ok - guias de remision'
-      }
-    });
+    if (this.OFFLINE_MODE) {
+      return dataGre as unknown as BodyResponse<T>;
+    } else {
+      return this.request<T>(`${this.base_url}`, {
+        method: 'GET',
+        headers: {
+          'x-mock-response-name': 'ok - guias de remision'
+        }
+      });
+    }
   }
 
   public async getByCodigoSeguimiento<T>(id: number): Promise<BodyResponse<T>> {
-    return this.request<T>(`${this.base_url}/${id}`, {
-      method: 'GET',
-      headers: {
-        'x-mock-response-name': 'ok - detalles de una guia de remision'
-      }
-    });
+    if (this.OFFLINE_MODE) {
+      return dataGreDetalle as unknown as BodyResponse<T>;
+    } else {
+      return this.request<T>(`${this.base_url}/${id}`, {
+        method: 'GET',
+        headers: {
+          'x-mock-response-name': 'ok - detalles de una guia de remision'
+        }
+      });
+    }
   }
 }
