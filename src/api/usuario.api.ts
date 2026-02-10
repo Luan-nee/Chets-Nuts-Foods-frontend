7a0d9e7dfccf8f2fa77b0030444370bc1ec6ac7f
Luan-nee <luandelsol54@gmail.com>
2026-02-09 20:06:53 -0500

    implementación del modo offline en el método login de la clase Usuario, importando un archivo JSON con los datos de respuesta simulados para el inicio de sesión exitoso. También se han importado otros archivos JSON relacionados con las respuestas de la API para futuras implementaciones.

--- src/api/usuario.api.ts
+++ src/api/usuario.api.ts
@@ -4,17 +4,24 @@ import BaseRequestApi from './BaseRequest.api';
 // importación de tipos
 import type { BodyResponse } from '../types/bodyResponse.type';
 import type { Credenciales } from '../types/usuario.type';
+import dataIniciarSesion from '../json/usuario/post-ok-iniciarSesion.json';
 
 export default class Usuario extends BaseRequestApi {
   private base_url = `${url_base}/sesion`;
 
   public async login<T>(credenciales: Credenciales): Promise<BodyResponse<T>> {
-    return this.request<T>(`${this.base_url}`, {
-      method: 'POST',
-      headers: {
-        'x-mock-response-name': 'ok - iniciar sesion'
-      },
-      body: JSON.stringify(credenciales)
-    });
+
+    if (this.OFFLINE_MODE) {
+      return dataIniciarSesion as unknown as BodyResponse<T>;
+    } else {
+      return this.request<T>(`${this.base_url}`, {
+        method: 'POST',
+        headers: {
+          'x-mock-response-name': 'ok - iniciar sesion'
+        },
+        body: JSON.stringify(credenciales)
+      });
+    }
+
   }
 }
