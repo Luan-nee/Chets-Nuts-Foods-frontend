02c5f5d61300919d1eb19513b3d78aa2d47467cd
Luan-nee <luandelsol54@gmail.com>
2026-02-14 20:03:22 -0500

    Realizando modificaciones a las variables de entorno para la comunicación con postman.

--- src/config/url_base.ts
+++ src/config/url_base.ts
@@ -1,2 +1,2 @@
 // usa la variables de entorno para la URL base del endpoint
-export const url_base: string = import.meta.env.VITE_URL_BASE_ENDPOINT || 'https://161a3dae-3154-4427-97ca-48ca81494472.mock.pstmn.io';
+export const url_base: string = import.meta.env.VITE_URL_BASE_ENDPOINT || 'https://95858b8a-f038-441a-9e94-48ca0994040c.mock.pstmn.io';
