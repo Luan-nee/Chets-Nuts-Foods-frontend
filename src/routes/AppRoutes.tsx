0cc373c07cc1c4cb33620b7a63048c1516138633
Luan-nee <luandelsol54@gmail.com>
2026-02-17 08:58:55 -0500

    Corrigiendo el error de las rutas de los componentes en AppRoutes.tsx

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,72 +1,89 @@
-import type { JSX } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "../context/AuthContext";
-import ListaGre from "../pages/administrador/ListaGre";
-import Establecimientos from "../pages/administrador/Establecimientos";
-import Login from "../pages/Login";
-import NotFound from "../pages/NotFound";
-import MainLayout from "../components/layouts/MainLayout";
-import PrivateRoute from "./PrivateRoute";
-import Trabajadores from "../pages/administrador/Trabajadores";
-import Vehiculos from "../pages/administrador/Vehiculos";
-import Configuraciones from "../pages/administrador/Configuraciones";
-
-type AppRouteType = {
-  path: string;
-  element: JSX.Element;
-};
-
-const appRoutes: AppRouteType[] = [
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/configuraciones",
-    element: <Configuraciones />,
-  },
-  {
-    path: "/guias",
-    element: <ListaGre />,
-  },
-  {
-    path: "/establecimientos",
-    element: <Establecimientos />,
-  },
-  {
-    path: "/trabajadores",
-    element: <Trabajadores />,
-  },
-  {
-    path: "/vehiculos",
-    element: <Vehiculos />,
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-];
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ListaGre from '../pages/administrador/ListaGre';
+import Establecimientos from '../pages/administrador/Establecimientos';
+import Login from '../pages/Login';
+import NotFound from '../pages/NotFound';
+import MainLayout from '../components/layouts/MainLayout';
+import PrivateRoute from './PrivateRoute';
+import { AuthProvider } from '../context/AuthContext';
+import Trabajadores from '../pages/administrador/Trabajadores';
+import Vehiculos from '../pages/administrador/Vehiculos';
+import Configuraciones from '../pages/administrador/Configuraciones';
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          {
-            appRoutes.map(({ path, element }, index) => (
-              <Route
-                key={index}
-                path={path}
-                element={
-                  <PrivateRoute>
-                    <MainLayout>
-                      {element}
-                    </MainLayout>
-                  </PrivateRoute>
-                }
-              />
-            ))
-          }
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <MainLayout>
+                  <ListaGre />
+                </MainLayout>
+              </PrivateRoute> 
+            }
+          />
+          <Route
+            path="/configuraciones"
+            element={
+              <PrivateRoute>
+                <MainLayout>
+                  <Configuraciones />
+                </MainLayout>
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/guias"
+            element={
+              <PrivateRoute>
+                <MainLayout>
+                  <ListaGre />
+                </MainLayout>
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/establecimientos"
+            element={
+              <PrivateRoute>
+                <MainLayout>
+                  <Establecimientos />
+                </MainLayout>
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/trabajadores"
+            element={
+              <PrivateRoute>
+                <MainLayout>
+                  <Trabajadores />
+                </MainLayout>
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/vehiculos"
+            element={
+              <PrivateRoute>
+                <MainLayout>
+                  <Vehiculos />
+                </MainLayout>
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={
+              <PrivateRoute>
+                <MainLayout>
+                  <NotFound />
+                </MainLayout>
+              </PrivateRoute>
+            } 
+          />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
