235869f6e86a38efb1c6118bf164e1e49bd7cd6b
Luan-nee <luandelsol54@gmail.com>
2026-03-15 03:45:17 -0500

    Implementación de todos los métodos en la API de productos.

--- src/api/producto.api.ts
+++ src/api/producto.api.ts
@@ -2,7 +2,8 @@ import { url_base } from "../config/url_base";
 // importación de clases
 import BaseRequestApi from './BaseRequest.api';
 // importación de tipos
-import type { BodyResponseWithPagination } from '../types/bodyResponse.type';
+import type { BodyResponseWithPagination, BodyResponse } from '../types/bodyResponse.type';
+import type { Producto } from '../features/productos/types/producto.type';
 import dataVehiculos from '../json/vehiculos/get-ok-listarVehiculos.json';
 
 export default class ProductoApi extends BaseRequestApi {
@@ -23,6 +24,46 @@ export default class ProductoApi extends BaseRequestApi {
   }
 
   /* REGISTRAR PRODUCTO */
+  public async registrarProducto<T>(body: Producto): Promise<BodyResponse<T>> {
+    if (this.OFFLINE_MODE) {
+      return dataVehiculos as unknown as BodyResponse<T>;
+    } else {
+      return this.request<BodyResponse<T>>(`${this.base_url}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      });
+    }
+  }
+
   /* INHABILITAR PRODUCTO */
+  public async inhabilitarProducto<T>(idProducto: number): Promise<BodyResponse<T>> {
+    if (this.OFFLINE_MODE) {
+      return dataVehiculos as unknown as BodyResponse<T>;
+    } else {
+      return this.request<BodyResponse<T>>(`${this.base_url}/inhabilitar/${idProducto}`, {
+        method: 'POST',
+        headers: {
+          'x-mock-response-name': 'ok - inhabilitar producto'
+        }
+      });
+    }
+  }
+
   /* MODIFICAR PRODUCTO */
+  public async modificarProducto<T>(idProducto: number, body: Producto): Promise<BodyResponse<T>> {
+    if (this.OFFLINE_MODE) {
+      return dataVehiculos as unknown as BodyResponse<T>;
+    } else {
+      return this.request<BodyResponse<T>>(`${this.base_url}/${idProducto}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      });
+    }
+  }
 }
---
+++ src/features/productos/types/producto.type.ts
@@ -0,0 +1,5 @@
+export type Producto = {
+  nombre: string;
+  peso: number;
+  unidadMedida: string;
+}
