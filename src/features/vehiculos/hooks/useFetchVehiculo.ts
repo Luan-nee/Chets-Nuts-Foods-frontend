b4ea36d47bcab3015cf16c4c6dacae49db3711e8
Luan-nee <luandelsol54@gmail.com>
2026-04-12 21:01:25 -0500

    Adaptando el hook useFetchVehiculo para recibir información del vehículo que retorna el service de vehiculo.

--- src/features/vehiculos/hooks/useFetchVehiculo.ts
+++ src/features/vehiculos/hooks/useFetchVehiculo.ts
@@ -2,11 +2,11 @@ import { useState, useEffect } from 'react';
 // importación de clases como servicios
 import { VehiculoService } from '../services/vehiculo.service';
 // importación de tipos
-import type { DetallesNumerados } from '../types/vehiculo.type';
+import type { DetallesVehiculo } from '../types/vehiculo.type';
 
 // Definimos el tipo de retorno de nuestro Hook
 interface FetchState {
-  data: DetallesNumerados | null;
+  data: DetallesVehiculo | null;
   isLoading: boolean;
   isError: boolean;
   fetchData: (idVehiculo: number) => Promise<void>;
@@ -15,7 +15,7 @@ interface FetchState {
 export const useFetchVehiculo = (idVehiculoProp: number): FetchState => {
   const vehiculoService = new VehiculoService();
   const [idVehiculoState, setIdVehiculoState] = useState<number>(idVehiculoProp);
-  const [data, setData] = useState<DetallesNumerados | null>(null);
+  const [data, setData] = useState<DetallesVehiculo | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isError, setIsError] = useState<boolean>(false);
 
