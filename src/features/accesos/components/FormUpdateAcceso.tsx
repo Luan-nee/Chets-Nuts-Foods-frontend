88a9f652fda31d0f5a3f089db102660113d3adaf
Luan-nee <luandelsol54@gmail.com>
2026-06-05 17:52:36 -0500

    Mejorando la funcionalidad del formulario para actualizar los datos de acceso de un empleado.

--- src/features/accesos/components/FormUpdateAcceso.tsx
+++ src/features/accesos/components/FormUpdateAcceso.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ArrowLeft } from "lucide-react";
 import InputPassword from "../../../components/ui/InputPassword";
 import InputText from "../../../components/ui/InputText";
@@ -6,12 +6,15 @@ import ContentPage from "../../../components/layouts/ContentPage";
 import ButtonCancelForm from "../../../components/ui/ButtonCancelForm";
 import ButtonSubmitForm from "../../../components/ui/ButtonSubmitForm";
 import InputSelect from "../../../components/ui/InputSelect";
+import Switch from "../../../components/ui/Switch";
+import ContentSectionProcess from "../../../components/layouts/ContentSectionProcess";
 import type { 
   UserRole as empleadoRol 
 } from "../../../types/constantes.type";
 import type { UpdateAcceso } from "../../../types/accesos.type";
 import { useFetchRoles } from "../hooks/useFetchRoles";
 import { useUpdateAcceso } from "../hooks/useUpdateAcceso";
+import { useFetchAcceso } from "../hooks/useFetchAcceso";
 import { InfoSuccess } from "../../../components/messages/InfoSuccess";
 import { InfoError } from "../../../components/messages/InfoError";
 import Loading from "../../../components/ui/Loading";
@@ -33,14 +36,33 @@ export default function FormCreate({ idEmpleado, setShowFormUpdateEmpleado }: Fo
     message: messageUpdateAcceso, 
     execute: updateAcceso 
   } = useUpdateAcceso();
+  const {
+    acceso,
+    isLoading: cargandoAcceso,
+    isError: errorAcceso,
+    message: messageAcceso,
+    execute: recargarAcceso
+  } = useFetchAcceso(idEmpleado);
 
-  const [ formData, setFormData ] = useState<UpdateAcceso>({
+  const [ formData, setFormData ] = useState<UpdateAcceso>({ 
     idacceso: idEmpleado,
     estado: false,
     correo: '',
     password: '',
-    tipos: 'ADMIN'
+    tipos: 'SIN ROL'
   });
+
+  useEffect(() => {
+    if (acceso) {
+      setFormData({
+        idacceso: idEmpleado || 0,
+        estado: acceso.estado || false,
+        correo: acceso.correo || '',
+        password: acceso.contra || '',
+        tipos: acceso.tipos || 'SIN ROL'
+      });
+    }
+  }, [idEmpleado]);
 
   const handleSubmit = async () => {
     // Validar campos requeridos
@@ -49,7 +71,6 @@ export default function FormCreate({ idEmpleado, setShowFormUpdateEmpleado }: Fo
       InfoError('Validación', 'Por favor completa todos los campos requeridos');
       return;
     }
-
     const response = await updateAcceso(formData);
     if (response && response.status === 'success') {
       InfoSuccess('Éxito', response.message || 'Acceso actualizado exitosamente');
@@ -81,83 +102,99 @@ export default function FormCreate({ idEmpleado, setShowFormUpdateEmpleado }: Fo
         </div>
       </div>
 
-      <div className="bg-gray-900 border border-gray-800 rounded-lg p-8 mx-8 my-6">
-        <div className="space-y-6">
-
-          {/* Row 1: Correo */}
-          <div className="grid grid-cols-1 gap-6">
-            <InputText
-              label="Correo electrónico"
-              value={formData.correo ?? ''}
-              htmlForm="correo"
-              onChange={(value) => setFormData(prev => ({ ...prev, correo: value }))}
-            />
-          </div>
-
-          {/* Row 2: Tip*/}
-          <div>
-            {
-              cargandoRoles ? (
-                <div className="flex justify-center items-center py-2">
-                  <Loading w={6} h={6} color="blue" />
-                </div>
-              ) : errorRoles ? (
-                <div className="flex justify-center items-center py-2">
-                  <p className="text-red-500">Error al cargar los roles.</p>
-                  <button
-                    className="ml-4 px-4 py-2 bg-red-600 text-white rounded"
-                    onClick={recargarRoles}
-                  >
-                    Reintentar
-                  </button>
-                </div>
-              ) : roles === null || roles.length === 0 ? (
-                <div>No hay roles registrados en el sistema.</div>
-              ) : (
-                <InputSelect 
-                  label="Tipo de acceso"
-                  options={roles.map(te => ({ label: te.rol, value: te.rol }))}
-                  placeholder="Seleccione el tipo de empleado"
-                  onSelect={(value) => setFormData(prev => ({ ...prev, tipos: value as empleadoRol }))}
-                  valueSelected={formData.tipos}
-                />
-              )
-            }
-          </div>
-
-          {/* Row 3: Contraseña (solo lectura) */}
-          <InputPassword
-            label="Contraseña generada automáticamente"
-            value={formData.password ?? ''}
-            htmlForm="password"
-            onChange={(e) => {
-              setFormData(prev => ({ ...prev, password: e }));
-            }} 
-          />
-        </div>
-
-        {/* Action Buttons */}
-        <div className="flex gap-4 mt-8">
-          <div className="flex flex-row gap-4 w-full justify-end">
-            <div className="flex flex-row gap-4">
-              <ButtonCancelForm
-                handleCancel={() => setShowFormUpdateEmpleado(false)}
-                isLoading={cargandoActualizarAcceso}
-                textButton="Cancelar"
-                color="red"
+      <ContentSectionProcess
+        isLoading={cargandoAcceso}
+        isError={errorAcceso}
+        textError={messageAcceso || "Error al cargar los datos del acceso"}
+        textButtonError="Reintentar"
+        fetchData={() =>recargarAcceso}
+      >
+        <div className="bg-gray-900 border border-gray-800 rounded-lg p-8 mx-8 my-6">
+          <div className="space-y-6">
+            {/* Row 1: Correo */}
+            <div className="grid grid-cols-1 gap-6">
+              <InputText
+                label="Correo electrónico"
+                value={formData.correo ?? ''}
+                htmlForm="correo"
+                onChange={(value) => setFormData(prev => ({ ...prev, correo: value }))}
               />
-              <ButtonSubmitForm
-                handleSubmit={handleSubmit}
-                isLoading={cargandoActualizarAcceso}
-                isError={!!messageUpdateAcceso}
-                textButton="Guardar Cambios"
-                textError={messageUpdateAcceso || "Error al guardar los cambios"}
-                color="blue"
+            </div>
+
+            {/* Row 2: Tipo de rol*/}
+            <div>
+              {
+                cargandoRoles ? (
+                  <div className="flex justify-center items-center py-2">
+                    <Loading w={6} h={6} color="blue" />
+                  </div>
+                ) : errorRoles ? (
+                  <div className="flex justify-center items-center py-2">
+                    <p className="text-red-500">Error al cargar los roles.</p>
+                    <button
+                      className="ml-4 px-4 py-2 bg-red-600 text-white rounded"
+                      onClick={recargarRoles}
+                    >
+                      Reintentar
+                    </button>
+                  </div>
+                ) : roles === null || roles.length === 0 ? (
+                  <div>No hay roles registrados en el sistema.</div>
+                ) : (
+                  <InputSelect 
+                    label="Tipo de acceso"
+                    options={roles.map(te => ({ label: te.rol, value: te.rol }))}
+                    placeholder="Seleccione el tipo de empleado"
+                    onSelect={(value) => setFormData(prev => ({ ...prev, tipos: value as empleadoRol }))}
+                    valueSelected={formData.tipos}
+                  />
+                )
+              }
+            </div>
+
+            {/* Row 3: Contraseña (solo lectura) */}
+            <InputPassword
+              label="Contraseña generada automáticamente"
+              value={formData.password ?? ''}
+              htmlForm="password"
+              onChange={(e) => {
+                setFormData(prev => ({ ...prev, password: e }));
+              }} 
+            />
+
+            {/* Row 4: Estado del acceso*/}
+            <div>
+              <Switch
+                inputName="estado"
+                activo={formData.estado ?? false}
+                handleInputChange={(field, value) => setFormData(prev => ({ ...prev, [field]: value }))}
               />
             </div>
           </div>
+
+          {/* Action Buttons */}
+          <div className="flex gap-4 mt-8">
+            <div className="flex flex-row gap-4 w-full justify-end">
+              <div className="flex flex-row gap-4">
+                <ButtonCancelForm
+                  handleCancel={() => setShowFormUpdateEmpleado(false)}
+                  isLoading={cargandoActualizarAcceso}
+                  textButton="Cancelar"
+                  color="red"
+                />
+                <ButtonSubmitForm
+                  handleSubmit={handleSubmit}
+                  isLoading={cargandoActualizarAcceso}
+                  isError={!!messageUpdateAcceso}
+                  textButton="Guardar Cambios"
+                  textError={messageUpdateAcceso || "Error al guardar los cambios"}
+                  color="blue"
+                />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </ContentSectionProcess>
     </ContentPage>
   )
 }
