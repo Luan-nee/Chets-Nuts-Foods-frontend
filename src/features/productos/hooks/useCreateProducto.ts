1718a1da9a710f5e4f444253e9778595ce9b891f
Luan-nee <luandelsol54@gmail.com>
2026-06-08 02:17:25 -0500

    Mejorando el llamado al endpoint para registrar nuevo producot.

--- src/features/productos/hooks/useCreateProducto.ts
+++ src/features/productos/hooks/useCreateProducto.ts
@@ -1,38 +1,50 @@
 import { useState } from 'react';
 // importación de clases como servicios
-import ProductoService from '../services/producto.service';
+import ProductoApi from '../../../api/producto.api';
 // importación de tipos
-import type { CrearProducto } from '../types/producto.type';
+import type { CreateProducto } from '../../../types/producto.type';
 
 // Definimos el tipo de retorno de nuestro Hook
 interface FetchState {
   isLoading: boolean;
   isError: boolean;
-  refresh: (body: CrearProducto) => void;
+  message: string;
+  execute: (body: CreateProducto) => void;
 }
 
 export const useCreateProducto = (): FetchState => {
-  const productoService = new ProductoService();
+  const producto_api = new ProductoApi();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isError, setIsError] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>("");
 
-  // Función asíncrona para obtener los datos
-  const refresh = async (bodyProducto: CrearProducto) => {
+  const registrarProducto = async (bodyProducto: CreateProducto) => {
     try {
       setIsLoading(true);
       setIsError(false);
-      const response = await productoService.registrarProducto(bodyProducto);
-      // Manejo de errores basado en el estado y el mensaje de la respuesta
-      if (response.status !== "success") {
-        throw new Error(response.message);
+      setMessage("");
+
+      const response = await producto_api.create(bodyProducto);
+      
+      if (response.status == "success") {
+        setMessage("Producto registrado exitosamente");
+        return response;
+      } else {
+        setIsError(true);
+        setMessage("Error al registrar el producto");
+        return response;
       }
     } catch (error) {
-      console.error("Fetch error: ", error);
       setIsError(true);
+      setMessage("Se produjo un error al registrar el producto en el frontend");
+      return {
+        status: "error",
+        message: "Error al registrar el producto"
+      }
     } finally {
       setIsLoading(false);
     }
   };
 
-  return { isLoading, isError, refresh};
+  return { isLoading, isError, message, execute: registrarProducto };
 };
