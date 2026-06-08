597396ffe14f1461a6911165da41311f9ca1ac4f
Luan-nee <luandelsol54@gmail.com>
2026-06-08 14:19:14 -0500

    Agregando custom hook para actualizar los productos.

--- src/features/productos/hooks/useUpdateProducto.ts
+++ src/features/productos/hooks/useUpdateProducto.ts
@@ -1,38 +1,51 @@
 import { useState } from 'react';
 // importación de clases como servicios
-import ProductoService from '../services/producto.service';
+import ProdutoApi from '../../../api/producto.api';
 // importación de tipos
-import type { ModificarProducto } from '../types/producto.type';
+import type { BodyResponse } from '../../../types/bodyResponse.type';
+import type { ResponseUpdateProducto, UpdateProducto } from '../../../types/producto.type';
 
 // Definimos el tipo de retorno de nuestro Hook
 interface FetchState {
   isLoading: boolean;
   isError: boolean;
-  refresh: (idProducto: number, body: ModificarProducto) => void;
+  message: string;
+  execute: (body: UpdateProducto) => Promise<BodyResponse<ResponseUpdateProducto>>;
 }
 
 export const useUpdateProducto = (): FetchState => {
-  const productoService = new ProductoService();
+  const producto_api = new ProdutoApi();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isError, setIsError] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>("");
 
-  // Función asíncrona para obtener los datos
-  const refresh = async (idProducto: number, bodyProducto: ModificarProducto) => {
+  const actualizarProducto = async (bodyProducto: UpdateProducto) => {
     try {
       setIsLoading(true);
       setIsError(false);
-      const response = await productoService.modificarProducto(idProducto, bodyProducto);
-      // Manejo de errores basado en el estado y el mensaje de la respuesta
-      if (response.status !== "success") {
-        throw new Error(response.message);
+      setMessage("");
+
+      const response = await producto_api.update(bodyProducto);
+      
+      if (response.status === 'success') {
+        setMessage('Producto actualizado exitosamente');
+        return response;
+      } else {
+        setIsError(true);
+        setMessage('Error al actualizar el producto');
+        return response;
       }
     } catch (error) {
-      console.error("Fetch error: ", error);
       setIsError(true);
+      setMessage('Se produjo un error al actualizar el producto en el frontend');
+      return {
+        status: "error",
+        message: "Error al actualizar el producto"
+      } as BodyResponse<ResponseUpdateProducto>;
     } finally {
       setIsLoading(false);
     }
   };
 
-  return { isLoading, isError, refresh};
+  return { isLoading, isError, message, execute: actualizarProducto };
 };
