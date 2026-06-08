4980a461b5ab4090c043017b9b6c483e4ecb92da
Luan-nee <luandelsol54@gmail.com>
2026-06-08 01:16:16 -0500

    implementando endpoint de gestión de productos.

--- src/api/producto.api.ts
+++ src/api/producto.api.ts
@@ -1,68 +1,42 @@
 import { url_base_production } from "../config/url_base";
+import type {
+  CreateProducto,
+  ResponseGetAllProductos,
+  ResponseCreateProducto,
+  UpdateProducto,
+  ResponseUpdateProducto,
+} from "../types/producto.type";
+import BaseRequestApi from "./BaseRequest.api";
 
-import BaseRequestApi from './BaseRequest.api';
-
-import type { BodyResponse, BodyResponseWithPagination } from '../types/bodyResponse.type';
+import type {
+  BodyResponse,
+  BodyResponseWithPagination,
+} from "../types/bodyResponse.type";
 
 export default class ProductoApi extends BaseRequestApi {
   private base_url_production = `${url_base_production}/api/productos`;
 
-  public async get<T>(page: number = 1): Promise<BodyResponseWithPagination<T>> {
-    const response: Response = await fetch(`${this.base_url_production}?page=${page}`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `bearer ${this.token}`
-      }
-    });
-
-    return response.json();
+  public async get(
+    page: number = 1,
+  ): Promise<BodyResponseWithPagination<ResponseGetAllProductos[]>> {
+    return this.GET(`${this.base_url_production}?page=${page}`) as Promise<
+      BodyResponseWithPagination<ResponseGetAllProductos[]>
+    >;
   }
 
-  public async registrarProducto<T>(body: unknown): Promise<BodyResponse<T>> {
-    const response: Response = await fetch(`${this.base_url_production}`, {
-      method: 'POST',
-      body: JSON.stringify(body),
-      headers: {
-        'Authorization': `bearer ${this.token}`,
-        "Content-Type": "application/json"
-      }
-    });
-
-    return response.json();
+  public async create(
+    body: CreateProducto,
+  ): Promise<BodyResponse<ResponseCreateProducto>> {
+    return this.POST(this.base_url_production, body) as Promise<
+      BodyResponse<ResponseCreateProducto>
+    >;
   }
 
-  public async modificarProducto<T>(id: number, body: unknown): Promise<BodyResponse<T>> {
-    const response: Response = await fetch(`${this.base_url_production}/${id}`, {
-      method: 'PATCH',
-      body: JSON.stringify(body),
-      headers: {
-        'Authorization': `bearer ${this.token}`,
-        "Content-Type": "application/json"
-      }
-    });
-
-    return response.json();
-  }
-
-  public async inhabilitarProducto<T>(id: number): Promise<BodyResponse<T>> {
-    const response: Response = await fetch(`${this.base_url_production}/${id}/deshabilitar`, {
-      method: 'PATCH',
-      headers: {
-        'Authorization': `bearer ${this.token}`
-      }
-    });
-
-    return response.json();
-  }
-
-  public async getDetallesProducto<T>(id: number): Promise<BodyResponse<T>> {
-    const response: Response = await fetch(`${this.base_url_production}/${id}`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `bearer ${this.token}`
-      }
-    });
-
-    return response.json();
+  public async update(
+    body: UpdateProducto,
+  ): Promise<BodyResponse<ResponseUpdateProducto>> {
+    return this.PATCH(`${this.base_url_production}`, body) as Promise<
+      BodyResponse<ResponseUpdateProducto>
+    >;
   }
 }
---
+++ src/types/producto.type.ts
@@ -0,0 +1,31 @@
+export type CreateProducto = {
+  nombre: string;
+  descripcion: string;
+}
+
+export type ResponseGetAllProductos = {
+  idproductdefect: number;
+  nombre: string;
+  descripcion: string;
+  fechacreacion: string;
+}
+
+export type ResponseCreateProducto = {
+  idproductdefect: number;
+  nombre: string;
+  descripcion: string;
+  fechacreacion: string;
+}
+
+export type UpdateProducto = {
+  idProductDefect: number;
+  nombre?: string;
+  descripcion?: string;
+}
+
+export type ResponseUpdateProducto = {
+  idproductodefect: number;
+  nombre: string;
+  descripcion: string;
+  fechacreacion: string;
+}
