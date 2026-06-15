edb61cb60334dd6cfb38f70cdb3ea647dfe85501
Luan-nee <luandelsol54@gmail.com>
2026-06-15 16:57:14 -0500

    Implementando diseño listado de clientes y formulario de creación de clientes.

--- src/pages/Clientes.tsx
+++ src/pages/Clientes.tsx
@@ -1,14 +1,34 @@
-import { useState, useEffect } from "react";
+import { Plus } from "lucide-react";
+import { useState } from "react";
 import TableClientes from "../features/clientes/components/TableClientes";
+import FormCreateCliente from "../features/clientes/components/FormCreateCliente";
 
 export default function Clientes() {
-  const [selectedClienteId, setSelectedClienteId] = useState<number | null>(null);
+  const [showFormCreate, setShowFormCreate] = useState<boolean>(false);
 
-  useEffect(() => {
-    if (selectedClienteId !== null) {
-      console.log("Cliente seleccionado ID:", selectedClienteId);
-    }
-  }, [selectedClienteId]);
+  return (
+    <div className="relative flex-1 flex flex-col">
+      {/* Header */}
+      <div className="bg-gray-900 border-b border-gray-800 px-8 py-6">
+        <div className="flex items-center justify-between">
+          <div>
+            <h2 className="text-3xl font-bold text-white mb-2">Gestión de clientes</h2>
+            <p className="text-sm text-gray-400">Gestiona la información de tus clientes.</p>
+          </div>
+          <button 
+            onClick={() => setShowFormCreate(true)}
+            className="flex items-center gap-2 bg-green-600 hover:bg-green-700 text-white px-5 py-2.5 rounded-lg font-medium transition-colors">
+            <Plus className="w-5 h-5" />
+            Nuevo Cliente
+          </button>
+        </div>
+      </div>
 
-  return <TableClientes setSelectedClienteId={setSelectedClienteId} />;
+      <TableClientes/>
+
+      { showFormCreate && (
+        <FormCreateCliente setShowFormCreate={setShowFormCreate} />
+      )}
+    </div>
+  );
 }
