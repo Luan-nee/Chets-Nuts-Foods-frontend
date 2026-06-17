422e906c29c011c4c9cfd97b4dcc609428b8fe46
Luan-nee <luandelsol54@gmail.com>
2026-06-17 18:31:38 -0500

    Agregando endpoint para registrar los datos y ubicación de la empresa con el endpoint del backend.

--- src/api/DatosEmpresa.api.ts
+++ src/api/DatosEmpresa.api.ts
@@ -1,13 +1,10 @@
 // import { url_base_postman } from "../../config/url_base";
 import { url_base_production } from "../config/url_base";
-
 // importación de clases
 import BaseRequestApi from './BaseRequest.api';
-
 // importación de tipos
 import type { BodyResponse } from '../types/bodyResponse.type';
-import type { UpdateDatosEmpresa } from '../types/datosEmpresa.type';
-
+import type { UpdateDatosEmpresa, InfoUbicacionState } from '../types/datosEmpresa.type';
 // importación de datos mock
 // ...
 
@@ -16,7 +13,10 @@ export default class DatosEmpresa extends BaseRequestApi {
   private base_url_production = `${url_base_production}/api/empresa`;
 
   /* setDatos */
-  public async setDatos(body: UpdateDatosEmpresa): Promise<BodyResponse<string>> {
+  public async registrarDatosEmpresarial(body: UpdateDatosEmpresa): Promise<BodyResponse<string>> {
     return this.POST<string>(`${this.base_url_production}`, body);
+  }
+  public async registraUbicacionEmpresa(body: InfoUbicacionState): Promise<BodyResponse<string>> {
+    return this.POST<string>(`${url_base_production}/api/establecimientos`, body);
   }
 }
--- src/types/datosEmpresa.type.ts
+++ src/types/datosEmpresa.type.ts
@@ -8,3 +8,18 @@ export type UpdateDatosEmpresa = {
   claveAcceso?: string;
   fechaVigenciaRegistroMtc: string;
 }
+
+export type InfoUbicacionState = {
+  nombreEstablecimiento: "chets nuts foods";
+  departamento: string;
+  provincia: string;
+  distrito: string;
+  direccion: string;
+  descripcion: "empresa principal";
+  latitud: string;
+  longitud: string;
+  ubigeo: string;
+  idResponsable: 1;
+  tipoEstado: "oficina";
+  codigoSunat?: string; // solo puede tener 5 caracteres. 
+};
