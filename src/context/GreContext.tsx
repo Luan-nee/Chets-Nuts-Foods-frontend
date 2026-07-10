7e50e175bdb4bfd794b5011aa331564a3c41a2b7
Luan-nee <luandelsol54@gmail.com>
2026-07-10 14:31:44 -0500

    Agregando estructura de datos en el contexto GreContext.tsx para guardar la información de la GRE.

--- src/context/GreContext.tsx
+++ src/context/GreContext.tsx
@@ -1,10 +1,24 @@
 import { createContext, useContext, useState, type ReactNode } from 'react';
+import type { EmitirGre } from '../types/gre.type';
+import type { CreatePaquete } from '../types/paquete.type';
+import type { ProductoEnPaquete, SalidaTransporteFormData } from '../types/constantes.type';
+
+interface DataEmitirGre {
+  emitir: EmitirGre;
+  paquete: CreatePaquete;
+  productosEnPaquete: ProductoEnPaquete[];
+  salidaTransporte: SalidaTransporteFormData;
+  idSalidaTransporte: number;
+  idPaquete: number;
+}
 
 interface GreContextType {
   idSalidaTransporte: number | null;
   setIdSalidaTransporte: (id: number | null) => void;
   idPaquete: number | null;
   setIdPaquete: (id: number | null) => void;
+  dataEmitirGre: DataEmitirGre;
+  setDataEmitirGre: React.Dispatch<React.SetStateAction<DataEmitirGre>>;
 }
 
 const GreContext = createContext<GreContextType | undefined>(undefined);
@@ -16,6 +30,33 @@ interface GreProviderProps {
 export const GreProvider = ({ children }: GreProviderProps) => {
   const [idSalidaTransporte, setIdSalidaTransporte] = useState<number | null>(null);
   const [idPaquete, setIdPaquete] = useState<number | null>(null);
+  const [dataEmitirGre, setDataEmitirGre] = useState<DataEmitirGre>({
+    emitir: {
+      motivoTraslado: '',
+      docDestinatario: '',
+      modalidadTransporte: '',
+      codigoTransporte: 0,
+    },
+    paquete: {
+      clave: '',
+      destino: 'sin definir',
+      idSalidaTransporte: 0,
+      idUsuario: 0,
+      idUsuarioDestino: 0,
+      montoCobrado: 0,
+    },
+    productosEnPaquete: [],
+    salidaTransporte: {
+      idChoferAcceso: 0,
+      idOrigenEstablecimiento: 0,
+      idDestinoEstablecimiento: 0,
+      idVehiculo: 0,
+      fechaSalida: '',
+      horasalida: '',
+    },
+    idSalidaTransporte: 0,
+    idPaquete: 0,
+  });
 
   return (
     <GreContext.Provider
@@ -24,6 +65,8 @@ export const GreProvider = ({ children }: GreProviderProps) => {
         setIdSalidaTransporte,
         idPaquete,
         setIdPaquete,
+        dataEmitirGre,
+        setDataEmitirGre,
       }}
     >
       {children}
