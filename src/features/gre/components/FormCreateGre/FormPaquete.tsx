f8c9f1091c057850fbe876e7c0ac9cd7bf3636b9
Luan-nee <luandelsol54@gmail.com>
2026-07-21 18:54:40 -0500

    Remplazando el componente TableSelectCliente por el componente InputSearch para seleccionar al empleado.
    La tarea pendiente es que el componente InputSearch muestre los parametros legibles para el usuarios.

--- src/features/gre/components/FormCreateGre/FormPaquete.tsx
+++ src/features/gre/components/FormCreateGre/FormPaquete.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import InputText from "../../../../components/ui/InputText";
-import TableSelectCliente from "../../../clientes/components/TableSelectCliente";
 import ButtonCancelForm from "../../../../components/ui/ButtonCancelForm";
 import ButtonSubmitForm from "../../../../components/ui/ButtonSubmitForm";
+import InputSearch from "../../../../components/ui/InputSearch";
 import { useRegistrarPaquete } from "../../../paquetes/hooks/useRegistrarPaquete";
+import { useFetchClientesSinCompras } from "../../../clientes/hooks/useFetchClientesSinCompras";
+import type { ResponseGetAll } from "../../../../types/usuarios.type"
 import type { CreatePaquete } from '../../../../types/paquete.type';
 import { useGreContext } from '../../../../context/GreContext';
 
@@ -12,7 +14,6 @@ export default function FormPaquete() {
     dataEmitirGre,
     setDataEmitirGre
   } = useGreContext();
-  const [, setIdCliente] = useState<number | null>(null);
   const [formData, setFormData] = useState<CreatePaquete>({
     clave: "",
     destino: "sin definir",
@@ -26,6 +27,9 @@ export default function FormPaquete() {
     isLoading: isLoadingPaquete,
     isError: isErrorPaquete,
   } = useRegistrarPaquete();
+  const {
+    clientes
+  } = useFetchClientesSinCompras();
 
   const syncPaquete = (nextValues: Partial<CreatePaquete>) => {
     setFormData((prev) => ({
@@ -43,20 +47,24 @@ export default function FormPaquete() {
   }, [formData, setDataEmitirGre]);
 
   return (
-    <div className="px-6 py-4 bg-gray-900 mx-6">
+    <div className="flex gap-4 flex-col px-6 py-4 bg-gray-900 mx-6">
+      <InputSearch<ResponseGetAll>
+        placeholder="Buscar cliente..."
+        atributes={['dniuser', 'nombres', 'apellidopaterno', 'apellidomaterno', 'rucuser']}
+        objets={clientes}
+        setObjetSelected={(cliente) => {
+          setFormData((prev) => ({
+            ...prev,
+            idUsuarioDestino: cliente.iduser,
+          }));
+        }}
+      />
       <InputText 
         htmlForm='clave de seguimiento'
         label='Clave de seguimiento'
         onChange={(value) => syncPaquete({ clave: value })}
         value={formData.clave}
       />
-      <TableSelectCliente 
-      selectIdCliente={setIdCliente} 
-      onChange={(setIdCliente) => {
-        syncPaquete({
-          idUsuarioDestino: setIdCliente || 0,
-        });
-      }} />
       <div className='flex gap-2'>
         <ButtonCancelForm 
           handleCancel={() => {}}
