bf9cf382a1733ecc25938dc91c8338d17c63d197
Luan-nee <luandelsol54@gmail.com>
2026-07-21 18:54:04 -0500

    Eliminando clases que impedia ver el contenido que sobresalía del componente.

--- src/components/layouts/ContentPage.tsx
+++ src/components/layouts/ContentPage.tsx
@@ -5,7 +5,7 @@ interface ContentPageProps {
 export default function ContentPage({children}: ContentPageProps) {
   return (
     <div className="absolute inset-0 z-50 bg-gray-950">
-      <div className="flex-1 bg-gray-950 overflow-auto">
+      <div className="flex-1 bg-gray-950">
         {children}
       </div>
     </div>
